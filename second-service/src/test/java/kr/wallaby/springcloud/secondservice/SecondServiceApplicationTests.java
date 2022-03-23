658f4fcfe39ca695ead5685193a016d4340ecf55
Wallaby <uym123@gmail.com>
2022-03-23 18:01:30 +0900

    1. Service-Discovery 추가 2. Service Project 추가 3. Api-Gateway 추가 4. First, Second Project 추가

---
+++ second-service/src/test/java/kr/wallaby/springcloud/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wallaby.springcloud.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service/src/test/java/kr/wallaby/springcloud/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wallaby.springcloud.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
