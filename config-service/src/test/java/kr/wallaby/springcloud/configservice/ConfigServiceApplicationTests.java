28a70a7dc2e3ac4ea6ee1fe1068664b40be6f834
Wallaby <uym123@gmail.com>
2022-04-11 23:24:44 +0900

    Spring Cloud Config 서버 생성

---
+++ config-service/src/test/java/kr/wallaby/springcloud/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.wallaby.springcloud.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
